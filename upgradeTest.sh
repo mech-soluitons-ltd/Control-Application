bf0a60fbe1f0a72a621806355473cd72694cd67b
Ironyrulez <chanmingyi@hotmail.com>
2021-10-12 15:27:03 -0400

    Added upgrade shell scripts
    - Shell scripts for testing and production environments added

---
+++ upgradeTest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Upgrade to v1.5.2
+# Disable and stop control panel service
+echo "Disabling and stopping control application service"
+systemctl disable c3p.service
+systemctl stop c3p.service
+
+# Clear old database files
+echo "Clearing database files"
+rm -f /home/pi/control/h2db/test*
+
+# Pull new JAR from repo
+echo "Pulling newest files"
+cd /home/pi/control
+git pull || true
+
+# Re-enable and restart the control panel service
+echo "Re-enabling and restarting control application service"
+systemctl enable c3p.service
+systemctl start c3p.service
+
+echo "Control upgrade script finished"
