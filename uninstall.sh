922baa6c80a24d3ba2cdc902555e86acf88f8173
Stephanos23 <stephen.luu2000@hotmail.com>
2021-10-18 12:36:46 -0400

    Remove c3pupgrade service

--- uninstall.sh
+++ uninstall.sh
@@ -7,8 +7,13 @@ if test -f "$FILE"; then
   sudo systemctl disable c3p.service
   sudo systemctl stop c3p.service
   sudo rm /etc/systemd/system/c3p.service
-  echo "Removed C3P services"
+  echo "Removed C3P service"
 fi
+
+if test -f "c3pUpgrade.service"; then
+  echo "C3P Upgrade Service Found"
+  sudo rm /etc/systemd/system/c3pUpgrade.service
+  echo "Removed C3P Upgrade service"
 
 if [ -d "/home/$USER/control/" ]; then
   echo "Control Folder Found"
